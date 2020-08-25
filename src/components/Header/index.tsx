0155e83f7cfbf09a7f568d490d7be00e7dfd2381
Jaime Duque <jaime.duque@estratek.com.co>
2020-08-24 22:41:54 -0500

    header component is updated

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,6 @@
 import React, { SFC } from 'react';
-import { Link } from 'react-router-dom';
-import woloxLogo from '../../assets/images/logo_full_color.svg';
 import peopleLogo from '../../assets/images/Ic_ilustra_Hero@3x.png';
-import { HOME } from '../../routes';
+import Burger from '../Burger';
 import './style.scss';
 
 export interface HeaderProps {
@@ -11,18 +9,9 @@ export interface HeaderProps {
 
 const Header: SFC<HeaderProps> = () => {
     return (
-        <div className="container">
-            <div className="menu_options">
-                <img alt="wolox logo" className="wolox_logo" src={woloxLogo} />
-                <Link to={HOME} className="home">Inicio</Link>
-                <Link to={HOME} className="home">Tecnologías</Link>
-                <Link to={HOME} className="home">Beneficios</Link>
-                <Link to={HOME} className="home">Requerimientos</Link>
-                <button className="login_button">
-                    Login
-                </button>
-            </div>
-            <div className="content">
+        window.screen.width <= 768 ? (<nav>
+            <Burger />
+            <div className="content" id="init">
                 <p className="content_text">
                     <span>Bienvenido a tu</span> <br />
                     <span className="txt_bold">Entrevista Técnica</span> en <br />
@@ -30,8 +19,23 @@ const Header: SFC<HeaderProps> = () => {
                 </p>
                 <img alt="people logo" className="people_logo" src={peopleLogo} />
             </div>
+        </nav>)
+            : (
+                <div className="container">
+                    <div className="menu_options">
+                        <Burger />
+                    </div>
+                    <div className="content">
+                        <p className="content_text">
+                            <span>Bienvenido a tu</span> <br />
+                            <span className="txt_bold">Entrevista Técnica</span> en <br />
+                            <span className="txt_green">Wolox</span>
+                        </p>
+                        <img alt="people logo" className="people_logo" src={peopleLogo} />
+                    </div>
 
-        </div>
+                </div>
+            )
     );
 }
 
