d8844607dee6a47c5de015294dd772c769e4c456
Jaime Duque <jaime.duque@estratek.com.co>
2020-08-24 22:42:36 -0500

    home page is updated

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,20 @@
 import React, { SFC } from 'react';
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import BodyContent from '../../components/BodyContent';
 
 export interface HomeProps {
-    
+
 }
- 
+
 const Home: SFC<HomeProps> = () => {
-    return (  
+    return (
         <>
             <Header />
+            <BodyContent />
+            <Footer />
         </>
     );
 }
- 
+
 export default Home;
