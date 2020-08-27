b4700595311418af6156108f4e1c65b13ef442b4
Jaime Duque <jaime.duque@estratek.com.co>
2020-08-27 18:16:25 -0500

    login route component is created

---
+++ src/components/LoginRoute/index.jsx
@@ -0,0 +1,24 @@
+import React, { useContext, useEffect } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { TECHLIST } from '../../routes';
+import AuthContext from '../../context/authentication/authContext';
+
+const LoginRoute = ({ component: Component, ...props }) => {
+
+    // Extract authentication information
+    const authContext = useContext(AuthContext);
+    const { token, userAuthenticated } = authContext;
+
+    useEffect(() => {
+        userAuthenticated();
+    }, []);
+
+    return (
+        <Route
+            {...props}
+            render={props => token ? (<Redirect to={TECHLIST} />) : (<Component {...props} />)}
+        />
+    );
+}
+
+export default LoginRoute;
