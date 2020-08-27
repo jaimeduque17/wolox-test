c806b2f6d32c472a95b432d8220e0024e6a4c2f4
Jaime Duque <jaime.duque@estratek.com.co>
2020-08-26 22:03:59 -0500

    types file is created

---
+++ src/types/index.jsx
@@ -0,0 +1,6 @@
+export const SHOW_ALERT = 'SHOW_ALERT';
+export const HIDE_ALERT = 'HIDE_ALERT';
+
+export const LOGIN_SUCCESSFUL = 'LOGIN_SUCCESSFUL';
+export const LOGIN_ERROR = 'LOGIN_ERROR';
+export const LOGOUT = 'LOGOUT';
