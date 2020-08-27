aae8496c5b001bdad917770f64dd0ccaf52f88c9
Jaime Duque <jaime.duque@estratek.com.co>
2020-08-27 01:03:31 -0500

    a private component is created

---
+++ src/components/PrivateRoute/index.jsx
@@ -0,0 +1,24 @@
+import React, { useContext, useEffect } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { LOGIN } from '../../routes';
+import AuthContext from '../../context/authentication/authContext';
+
+const PrivateRoute = ({ component: Component, ...props }) => {
+
+    // Extract authentication information
+    const authContext = useContext(AuthContext);
+    const { auth, token, userAuthenticated } = authContext;
+
+    useEffect(() => {
+        userAuthenticated();
+    }, []);
+
+    return (
+        <Route
+            {...props}
+            render={props => !auth && !token ? (<Redirect to={LOGIN} />) : (<Component {...props} />)}
+        />
+    );
+}
+
+export default PrivateRoute;
