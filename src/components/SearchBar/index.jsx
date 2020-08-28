c2a853d5b063fd0e804cd44ac951e448425591f6
Jaime Duque <jaime.duque@estratek.com.co>
2020-08-28 18:32:49 -0500

    search bar component is created

---
+++ src/components/SearchBar/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import './style.scss';
+
+const SearchBar = ({ value, handleChange }) => {
+
+    // translations
+    const { t } = useTranslation();
+
+    return (
+        <div className="wrap_search">
+            <div className="search">
+                <input className="searchTerm" placeholder={t('Search.placeholder')} value={value} onChange={handleChange} />
+                <div class="searchImg">
+                🔍
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SearchBar;
