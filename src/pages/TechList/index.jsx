863559610a9a5eba002d442ad098c8e965e5068b
Jaime Duque <jaime.duque@estratek.com.co>
2020-08-28 18:33:38 -0500

    search bar component is implemented

--- src/pages/TechList/index.jsx
+++ src/pages/TechList/index.jsx
@@ -1,17 +1,19 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import woloxLogo from '../../assets/images/logo_full_color.svg';
 import AuthContext from '../../context/authentication/authContext';
 import TechContext from '../../context/technologies/techContext';
+import Languages from '../../components/Language';
+import SearchBar from '../../components/SearchBar';
 import Technology from '../../components/Technology';
 import { HOME } from '../../routes';
 import './style.scss';
 
 const TechList = () => {
 
-     // translations
-     const { t } = useTranslation();
+    // translations
+    const { t } = useTranslation();
 
     // Extract values of the context
     const techContext = useContext(TechContext);
@@ -20,15 +22,38 @@ const TechList = () => {
     const authContext = useContext(AuthContext);
     const { logOut } = authContext;
 
+    const [word, setWord] = useState('');
+
+    const [filterDisplay, setFilterDisplay] = useState([]);
+
     // Get techs when charge the component
     useEffect(() => {
         getTechs();
         // eslint-disable-next-line
     }, []);
 
+    const handleChange = e => {
+        setWord(e);
+        let oldList = techs.map(tech => {
+            return { logo: tech.logo, tech: tech.tech.toLowerCase(), year: tech.year, author: tech.author, license: tech.license, language: tech.language, type: tech.type.toLowerCase() }
+        });
+        if (word !== "") {
+            let newList = [];
+
+            newList = oldList.filter(tech =>
+                tech.tech.includes(word.toLowerCase()) ||
+                tech.type.includes(word.toLowerCase())
+            );
+            setFilterDisplay(newList);
+        } else {
+            setFilterDisplay(techs);
+        }
+    }
+
     return (
         <>
             <div className="header_techlist">
+                <Languages />
                 <Link className="list_to_home" to={HOME}><img alt="wolox logo" className="img_wlx_list" src={woloxLogo} /></Link>
                 <button
                     className="logout_btn"
@@ -36,15 +61,11 @@ const TechList = () => {
                 >{t('Common.logout')}
                 </button>
             </div>
+            <SearchBar value={word} handleChange={e => handleChange(e.target.value)} />
             <div className="list_container">
-                {techs.map(tech => (
-                    <div className="tech_list_content"
-                        key={tech.tech}>
-                        <Technology
-                            technology={tech}
-                        />
-                    </div>
-                ))}
+                <Technology
+                    technologies={word.length < 1 ? techs : filterDisplay}
+                />
             </div>
         </>
     );
