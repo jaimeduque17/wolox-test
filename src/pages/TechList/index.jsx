5291f8ef4454d8994e35728e5a9458d1d9223256
Jaime Duque <jaime.duque@estratek.com.co>
2020-08-27 21:43:12 -0500

    tech list page is updated and styles are added

--- src/pages/TechList/index.jsx
+++ src/pages/TechList/index.jsx
@@ -1,21 +1,49 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import woloxLogo from '../../assets/images/logo_full_color.svg';
 import AuthContext from '../../context/authentication/authContext';
+import TechContext from '../../context/technologies/techContext';
+import Technology from '../../components/Technology';
+import { HOME } from '../../routes';
+import './style.scss';
 
 const TechList = () => {
 
-    // Extract authentication information
-    const authContext = useContext(AuthContext);
-    const { userAuthenticated } = authContext;
+    // Extract values of the context
+    const techContext = useContext(TechContext);
+    const { getTechs, techs } = techContext;
 
+    const authContext = useContext(AuthContext);
+    const { logOut } = authContext;
+
+    // Get techs when charge the component
     useEffect(() => {
-        userAuthenticated();
+        getTechs();
+        // eslint-disable-next-line
     }, []);
 
-    return (  
-        <div>
-            TechList
-        </div>
+    return (
+        <>
+            <div className="header_techlist">
+                <Link className="list_to_home" to={HOME}><img alt="wolox logo" className="img_wlx_list" src={woloxLogo} /></Link>
+                <button
+                    className="logout_btn"
+                    onClick={() => logOut()}
+                >Logout
+                </button>
+            </div>
+            <div className="list_container">
+                {techs.map(tech => (
+                    <div className="tech_list_content"
+                        key={tech.tech}>
+                        <Technology
+                            technology={tech}
+                        />
+                    </div>
+                ))}
+            </div>
+        </>
     );
 }
- 
+
 export default TechList;
