d9c31a6ab7b86ce7a3216e05f785b1860ff9a6d3
Jaime Duque <jaime.duque@estratek.com.co>
2020-08-27 21:16:30 -0500

    technologies types are added

--- src/types/index.jsx
+++ src/types/index.jsx
@@ -4,3 +4,6 @@ export const HIDE_ALERT = 'HIDE_ALERT';
 export const LOGIN_SUCCESSFUL = 'LOGIN_SUCCESSFUL';
 export const LOGIN_ERROR = 'LOGIN_ERROR';
 export const LOGOUT = 'LOGOUT';
+
+export const GET_TECHS = 'GET_TECHS';
+export const ERROR_TECH = 'ERROR_TECH';
