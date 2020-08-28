fd0e8e648824df17f5dd99d91828f7e867de7863
Jaime Duque <jaime.duque@estratek.com.co>
2020-08-28 18:33:15 -0500

    technology component is updated

--- src/components/Technology/index.jsx
+++ src/components/Technology/index.jsx
@@ -2,25 +2,27 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import './style.scss';
 
-const Technology = ({ technology }) => {
+const Technology = ({ technologies }) => {
 
-     // translations
-     const { t } = useTranslation();
+    // translations
+    const { t } = useTranslation();
 
     return (
-        <div className="tech_content">
-            <div className="container_tech_img">
-                <img src={technology.logo} className="img_tech" alt="technology logo" />
+        technologies && technologies.map((tech, i) => (
+            <div className="tech_content" key={i}>
+                <div className="container_tech_img">
+                    <img src={tech.logo} className="img_tech" alt="technology logo" />
+                </div>
+                <div className="tech_properties">
+                    <p><span className="bold_txt">{t('List.tech')} </span>{tech.tech}</p>
+                    <p><span className="bold_txt">{t('List.year')} </span>{tech.year}</p>
+                    <p><span className="bold_txt">{t('List.author')} </span>{tech.author}</p>
+                    <p><span className="bold_txt">{t('List.license')} </span>{tech.license}</p>
+                    <p><span className="bold_txt">{t('List.language')} </span>{tech.language}</p>
+                    <p><span className="bold_txt">{t('List.type')} </span>{tech.type}</p>
+                </div>
             </div>
-            <div className="tech_properties">
-                <p><span className="bold_txt">{t('List.tech')} </span>{technology.tech}</p>
-                <p><span className="bold_txt">{t('List.year')} </span>{technology.year}</p>
-                <p><span className="bold_txt">{t('List.author')} </span>{technology.author}</p>
-                <p><span className="bold_txt">{t('List.license')} </span>{technology.license}</p>
-                <p><span className="bold_txt">{t('List.language')} </span>{technology.language}</p>
-                <p><span className="bold_txt">{t('List.type')} </span>{technology.type}</p>
-            </div>
-        </div>
+        ))
     );
 }
 
